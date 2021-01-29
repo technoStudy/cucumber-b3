f048885e066c8467bb07a669651946eb7a8b93e4
daulet2030 <daulet2030@gmail.com>
2021-01-29 08:51:32 +0600

    implementing google search feature using selenium

--- src/test/java/StepDefinitions/GoogleSearchSteps.java
+++ src/test/java/StepDefinitions/GoogleSearchSteps.java
@@ -3,25 +3,42 @@ package StepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
+import java.util.List;
+
 public class GoogleSearchSteps {
+
+    private WebDriver driver;
+
     @Given("I navigate to Google")
     public void iNavigateToGoogle() {
         System.out.println("I am navigating to google website");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://google.com");
     }
 
     @When("I enter {string} in search box")
     public void iEnterInSearchBox(String searchText) {
         System.out.println("Entering " + searchText + " in search box");
-//        throw new RuntimeException(); // will not continue if a step fails
+        driver.findElement(By.cssSelector("input[name=\"q\"]")).sendKeys(searchText);
     }
 
     @Then("I see {string} results")
     public void iSeeResults(String resultText) {
         System.out.println("I must see results containing " + resultText);
-        String actualText = "Get it from selenium driver findBy";
-        Assert.assertEquals(actualText, resultText);
+        List<WebElement> elements = driver.findElements(By.cssSelector("form ul > li"));
+        for (WebElement result : elements) {
+            String actualText = result.getText();
+            boolean contains = actualText.contains(resultText);
+            Assert.assertTrue(contains);
+        }
     }
 
     @Given("I launch web browser,")
--- src/test/resources/features/google.feature
+++ src/test/resources/features/google.feature
@@ -1,12 +1,11 @@
 Feature: Google Search
 
   Scenario: Search for Cucumber in Google
-    Given I launch web browser,
     Given I navigate to Google
     When I enter "Cucumber" in search box
-    Then I see "Cucumber" results
+    Then I see "cucumber" results
 
   Scenario: Search for Gherkin in Google
     Given I navigate to Google
     When I enter "Gherkin" in search box
-    Then I see "Gherkin" results
+    Then I see "gherkin" results
