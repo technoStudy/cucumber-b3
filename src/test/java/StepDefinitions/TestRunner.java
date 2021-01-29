e699e9e8ca3b271873e17c187486a8963b88e407
daulet2030 <daulet2030@gmail.com>
2021-01-29 08:21:28 +0600

    adding testNg TestRunner

---
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -0,0 +1,11 @@
+package StepDefinitions;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features/google.feature",
+        glue = "StepDefinitions"
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
