b695e8e7157b1082e00208018160643c1c6807e9
daulet2030 <daulet2030@gmail.com>
2021-01-30 08:12:39 +0600

    step keywords

---
+++ src/test/java/StepDefinitions/StepKeywordSteps.java
@@ -0,0 +1,38 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepKeywordSteps {
+    @And("some condition")
+    public void someCondition() {
+        
+    }
+
+    @And("I get the result")
+    public void iGetTheResult() {
+
+    }
+
+    @Given("one thing")
+    public void oneThing() {
+    }
+
+    @And("another thing")
+    public void anotherThing() {
+    }
+
+    @When("I do action")
+    public void iDoAction() {
+    }
+
+    @Then("I don't get something else")
+    public void iDonTGetSomethingElse() {
+    }
+
+    @And("one more thing")
+    public void oneMoreThing() {
+    }
+}
---
+++ src/test/resources/features/stepKeywords.feature
@@ -0,0 +1,14 @@
+Feature: Testing step keywords
+
+  Scenario: Random position for keyword
+    And some condition
+    Given some condition
+    And I get the result
+
+    Scenario: correct usage
+      Given one thing
+      And another thing
+      And one more thing
+      When I do action
+      Then I get the result
+      But I don't get something else
