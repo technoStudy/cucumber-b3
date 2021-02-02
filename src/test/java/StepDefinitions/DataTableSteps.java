3a183314293653c339785d8cc1a62af0dceaf431
daulet2030 <daulet2030@gmail.com>
2021-02-02 08:20:02 +0600

    data table list of maps

--- src/test/java/StepDefinitions/DataTableSteps.java
+++ src/test/java/StepDefinitions/DataTableSteps.java
@@ -1,9 +1,11 @@
 package StepDefinitions;
 
+import Models.GenericObjectClass;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 
 import java.util.List;
+import java.util.Map;
 
 public class DataTableSteps {
     @Given("I have a list of values")
@@ -31,4 +33,26 @@ public class DataTableSteps {
     }
 
 
+    @Given("I have a map of value")
+    public void iHaveAMapOfValue(DataTable table) {
+        Map<String, Integer> map = table.asMap(String.class, Integer.class);
+        System.out.println(map);
+    }
+
+    @Given("I have a list of maps")
+    public void iHaveAListOfMaps(DataTable table) {
+        List<Map<String, String>> maps = table.asMaps();
+
+        for(Map<String, String> map : maps) {
+            System.out.println(map);
+        }
+    }
+
+    @Given("I have a list of objects")
+    public void iHaveAListOfObjects(List<GenericObjectClass> list) {
+        // TODO: fix this with data table type
+        for (GenericObjectClass item : list ) {
+            System.out.println(item);
+        }
+    }
 }
--- src/test/resources/features/dataTable.feature
+++ src/test/resources/features/dataTable.feature
@@ -8,10 +8,31 @@ Feature: Testing data table
 
   Scenario: List scenario
     Given I have a list
-      | 1      | 2      | 3      |
-      | 4      | 5      | 6      |
+      | 1 | 2 | 3 |
+      | 4 | 5 | 6 |
 
   Scenario: List of list scenario
     Given I have a list of lists
-      | 1      | 2      | 3      |
-      | 4      | 5      | 6      |
+      | 1 | 2 | 3 |
+      | 4 | 5 | 6 |
+
+  Scenario: Map scenario
+    Given I have a map of value
+      | field1 | 1 |
+      | field2 | 2 |
+      | field3 | 3 |
+      | field4 | 4 |
+
+  Scenario: List of Maps scenario
+    Given I have a list of maps
+      | field1 | field2 | field3 | field4 |
+      | 11     | 12     | 13     | 14     |
+      | 21     | 22     | 23     | 24     |
+      | 31     | 32     | 33     | 34     |
+
+  Scenario: List of Objects scenario
+    Given I have a list of objects
+      | field1 | field2 | field3 | field4 |
+      | 11     | 12     | 13     | 14     |
+      | 21     | 22     | 23     | 24     |
+      | 31     | 32     | 33     | 34     |
