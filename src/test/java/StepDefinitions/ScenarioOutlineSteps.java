e57a9cfef420612c44bd8e80cafc71586c9ddfac
daulet2030 <daulet2030@gmail.com>
2021-02-02 06:31:26 +0600

    scenario outline test

--- src/test/java/StepDefinitions/ScenarioOutlineSteps.java
+++ src/test/java/StepDefinitions/ScenarioOutlineSteps.java
@@ -3,20 +3,24 @@ package StepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 public class ScenarioOutlineSteps {
+    private int startingCucumbers;
+    private int eatingCucumbers;
+
     @Given("there are {int} cucumbers")
     public void thereAreStartCucumbers(int start) {
-        System.out.println(start); // store start in a variable
+        startingCucumbers = start;
     }
 
     @When("I eat {int} cucumbers")
     public void iEatEatCucumbers(int eat) {
-        System.out.println(eat); // store eat in a variable
+        eatingCucumbers = eat;
     }
 
     @Then("I should have {int} cucumbers")
     public void iShouldHaveLeftCucumbers(int left) {
-        System.out.println(left); // check that start - eat = left
+        Assert.assertEquals(startingCucumbers - eatingCucumbers, left);
     }
 }
