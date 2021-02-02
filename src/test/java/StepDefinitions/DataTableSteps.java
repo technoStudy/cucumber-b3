2b7e3c435f07af9d9fd3d9b3b963cc0a1ef457a2
daulet2030 <daulet2030@gmail.com>
2021-02-02 07:44:49 +0600

    data table list of lists

--- src/test/java/StepDefinitions/DataTableSteps.java
+++ src/test/java/StepDefinitions/DataTableSteps.java
@@ -3,9 +3,22 @@ package StepDefinitions;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 
+import java.util.List;
+
 public class DataTableSteps {
     @Given("I have a list of values")
     public void iHaveAListOfValues(DataTable table) {
         System.out.println(table);
+    }
+
+    @Given("I have a list of lists")
+    public void iHaveAListOfLists(DataTable table) {
+        List<List<String>> lists = table.asLists();
+        for (List<String> list : lists) {
+            for (String item:list) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/test/resources/features/dataTable.feature
+++ src/test/resources/features/dataTable.feature
@@ -5,3 +5,8 @@ Feature: Testing data table
       | value1 | value2 | value3 |
       | 1      | 2      | 3      |
       | 4      | 5      | 6      |
+
+  Scenario: List of list scenario
+    Given I have a list of lists
+      | 1      | 2      | 3      |
+      | 4      | 5      | 6      |
