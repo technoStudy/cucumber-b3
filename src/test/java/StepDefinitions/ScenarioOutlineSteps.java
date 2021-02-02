616a74efbfca82cc164d6e938b9d36cf08c3af57
daulet2030 <daulet2030@gmail.com>
2021-02-02 06:22:48 +0600

    scenario outline

--- src/test/java/StepDefinitions/ScenarioOutlineSteps.java
+++ src/test/java/StepDefinitions/ScenarioOutlineSteps.java
@@ -7,16 +7,16 @@ import io.cucumber.java.en.When;
 public class ScenarioOutlineSteps {
     @Given("there are {int} cucumbers")
     public void thereAreStartCucumbers(int start) {
-        System.out.println(start);
+        System.out.println(start); // store start in a variable
     }
 
     @When("I eat {int} cucumbers")
     public void iEatEatCucumbers(int eat) {
-        System.out.println(eat);
+        System.out.println(eat); // store eat in a variable
     }
 
     @Then("I should have {int} cucumbers")
     public void iShouldHaveLeftCucumbers(int left) {
-        System.out.println(left);
+        System.out.println(left); // check that start - eat = left
     }
 }
