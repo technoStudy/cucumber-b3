17bd85a2c70c81b9cdd635eb160fefcf2706c4a7
daulet2030 <daulet2030@gmail.com>
2021-02-03 07:41:48 +0600

    filtering out hooks

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -10,18 +10,18 @@ public class Hooks {
         System.out.println("Before scenario " + scenario.getName() + "!");
     }
 
-    @After
+    @After()
     public void tearDown(Scenario scenario) {
         System.out.println("After scenario " + scenario.getName() + "!");
         System.out.println(scenario.getStatus());
     }
 
-    @BeforeStep
+    @BeforeStep("@scenario3")
     public void beforeEachStep(Scenario scenario) {
         System.out.println("Before step");
     }
 
-    @AfterStep
+    @AfterStep("@scenario3")
     public void afterEachStep(Scenario scenario){
         System.out.println("After step");
     }
--- src/test/java/StepDefinitions/TestGroup1Runner.java
+++ src/test/java/StepDefinitions/TestGroup1Runner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = {"src/test/resources/features/google.feature", "src/test/resources/features/backgroundKeyword.feature"},
         glue = "StepDefinitions",
         tags = "@group1"
 )
