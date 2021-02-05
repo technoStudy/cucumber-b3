72eae9be7f1b42770f9f410c2710e82d33b1151d
daulet2030 <daulet2030@gmail.com>
2021-02-05 06:29:57 +0600

    using plugin to generate html report

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,11 +1,12 @@
-package StepDefinitions;
+package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features/google.feature:8",
-        glue = "StepDefinitions"
+        glue = "StepDefinitions",
+        plugin = {"html:target/report/index.html"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
