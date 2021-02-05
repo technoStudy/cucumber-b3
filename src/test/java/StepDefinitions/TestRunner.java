4724eb5e0f26e517ec7fc351c78a5a4af1529abc
daulet2030 <daulet2030@gmail.com>
2021-02-05 06:11:24 +0600

    running specific scenario

--- src/test/java/StepDefinitions/TestRunner.java
+++ src/test/java/StepDefinitions/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "src/test/resources/features/google.feature",
+        features = "src/test/resources/features/google.feature:8",
         glue = "StepDefinitions"
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
